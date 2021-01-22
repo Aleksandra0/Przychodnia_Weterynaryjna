326a9297a8cdcd45053a3da5b6853092c782142b
Aleksandra0 <66222827+Aleksandra0@users.noreply.github.com>
2021-01-22 21:27:30 +0100

    Add files via upload

---
+++ Projekt_Przychodnia_weterynaryjna/IZarzadzanie_pacjentami.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt_Przychodnia_weterynaryjna
+{
+    /// <summary>
+    ///   Interface for adding, deleting patients and clearing the list of patients
+    /// </summary>
+    interface IZarzadzanie_pacjentami
+    {
+        void Dodaj_pacjenta(Pacjent p);
+        void Usun_pacjenta(int id);
+        void Wyczysc_liste();
+    }
+}
